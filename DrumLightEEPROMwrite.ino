2d537a29f7f5377d6540b5f65a3797101bd27f5d
Joseph Rork <jrork1@gmail.com>
2021-01-09 23:04:11 -0500

    Initial submit

---
+++ DrumLightEEPROMwrite.ino
@@ -0,0 +1,27 @@
+/*
+ * Tool to write initial values to EEPROM for DrumLight program
+*/
+
+#include <EEPROM.h>
+#include "drums.h"
+
+drumLight myDrumLight = {Bass1, 65523, 80, 20, 0};
+
+void setup()
+{
+    char* myDrumLightBytes = reinterpret_cast<char*>(&myDrumLight);
+    const uint32_t myDrumLightSize = sizeof(myDrumLight);
+
+  EEPROM.begin(myDrumLightSize);  //Initialize EEPROM
+
+  for(int index = 0; index < myDrumLightSize; index++){
+    EEPROM.write(index, myDrumLightBytes[index]);
+  }
+  
+  EEPROM.commit();    //Store data to EEPROM
+}
+
+void loop()
+{
+  delay(1000);
+}
